f485075203a26dd89ef20d7fd63d25bb0807081b
schen747 <155417163+schen747@users.noreply.github.com>
2024-01-14 12:59:31 -0600

    add model_name = 'gpt-3.5-turbo' for OpenAI ( )

--- streamLit_textField.py
+++ streamLit_textField.py
@@ -52,7 +52,7 @@ def main():
       if user_question:
         docs = knowledge_base.similarity_search(user_question)
         
-        llm = OpenAI()
+        llm = OpenAI(model_name = 'gpt-3.5-turbo')
         chain = load_qa_chain(llm, chain_type="stuff")
         with get_openai_callback() as cb:
           response = chain.run(input_documents=docs, question=user_question)
